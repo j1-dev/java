86919a6228de34d429608754d25096f95ba65eda
j1-dev <aephyxen@gmail.com>
2023-04-21 08:25:40 +0200

    21 abr

---
+++ ficheros/proyectos/ejerciciofiles/src/App.java
@@ -0,0 +1,17 @@
+package ficheros.proyectos.ejerciciofiles.src;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        FileReader fr = new FileReader("");
+        BufferedReader br = new BufferedReader(fr, 0);
+
+        String line = br.readLine();
+        while (line != "") {
+            System.out.println(line);
+            line = br.readLine();
+        }
+    }
+}
