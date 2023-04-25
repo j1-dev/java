681553b800b5de179bb27ca1df9f8fe132e75625
j1-dev <aephyxen@gmail.com>
2023-04-25 18:05:19 +0200

    25 abr

--- ficheros/proyectos/tareaMunicipios/src/App.java
+++ ficheros/proyectos/tareaMunicipios/src/App.java
@@ -1,5 +1,15 @@
+import java.util.ArrayList;
+
+import clases.LeerInformacion;
+import clases.Municipio;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        ArrayList<Municipio> municipios = new ArrayList<>();
+
+        municipios = LeerInformacion.LeerFicheroMunicipio(2000);
+
+        System.out.println(municipios.toString());
+
     }
 }
--- ficheros/proyectos/tareaMunicipios/src/clases/LeerInformacion.java
+++ ficheros/proyectos/tareaMunicipios/src/clases/LeerInformacion.java
@@ -5,9 +5,10 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 
-public abstract class LeerInformacion {
-  public ArrayList<Municipio> LeerFicheroMunicipio(Integer año) throws IOException {
+public class LeerInformacion {
+  public static ArrayList<Municipio> LeerFicheroMunicipio(Integer year) throws IOException {
     ArrayList<Municipio> res = new ArrayList<>();
     String rutaCSV = "src/recursos/PoblacionMunicipiosMalaga.csv";
 
@@ -23,12 +24,12 @@ public abstract class LeerInformacion {
       while (linea != null) {
         String[] splitLine = linea.split("[\\s;]+");
 
-        int cp = Integer.valueOf(splitLine[0]);
+        Integer cp = Integer.valueOf(splitLine[0]);
         String nombre = splitLine[1];
         String sexo = splitLine[2];
-        int anio = Integer.valueOf(splitLine[3]);
+        Integer anio = Integer.valueOf(splitLine[3]);
         String numConPuntos;
-        int poblacion;
+        Integer poblacion;
 
         if (splitLine.length == 5) {
           numConPuntos = splitLine[4];
@@ -38,7 +39,7 @@ public abstract class LeerInformacion {
           poblacion = -1;
         }
 
-        if (sexo.equals("Total")) {
+        if (sexo.equals("Total") && year == anio) {
           Municipio m = new Municipio(nombre, poblacion, anio, cp);
           res.add(m);
         }
@@ -46,20 +47,38 @@ public abstract class LeerInformacion {
       }
     } catch (IOException e) {
       System.out.println(e.toString());
-    } finally {
-      fr.close();
-      br.close();
     }
+
     return res;
   }
 
-  public Municipio buscarMunicipio(ArrayList<Municipio> lista, String nombre, int anio) {
+  public static Municipio buscarMunicipio(ArrayList<Municipio> lista, String nombre, Integer anio) {
     lista.sort((arg0, arg1) -> {
       return arg0.getAnio() - arg1.getAnio() + arg0.getNombre().compareTo(arg1.getNombre());
     });
+
     Municipio m = new Municipio(nombre, 0, anio, 0);
-    return lista.get(Collections.binarySearch(lista, m, ((arg0, arg1) -> {
+
+    return lista.get(Collections.binarySearch(lista, m, (arg0, arg1) -> {
       return (arg0.getNombre().compareTo(arg1.getNombre()) + arg0.getAnio() - arg1.getAnio());
-    })));
+    }));
+  }
+
+  public static HashMap<String, Integer> IncrementoPoblacion(ArrayList<Municipio> coleccionMunicipios, int year1,
+      int year2) {
+    HashMap<String, Integer> res = new HashMap<>();
+
+    for (Municipio municipio : coleccionMunicipios) {
+      Municipio municipioYear1 = buscarMunicipio(coleccionMunicipios, municipio.getNombre(), year1);
+      Municipio municipioYear2 = buscarMunicipio(coleccionMunicipios, municipio.getNombre(), year2);
+
+      int poblacionYear1 = municipioYear1.getPoblacion();
+      int poblacionYear2 = municipioYear2.getPoblacion();
+      int diff = poblacionYear1 - poblacionYear2;
+
+      res.put(municipio.getNombre(), diff);
+    }
+
+    return res;
   }
 }
