e4109919840f1c1baf7a80899290539e278132d4
j1-dev <aephyxen@gmail.com>
2023-04-26 12:41:09 +0200

    26 abr

--- ficheros/proyectos/tareaMunicipios/src/App.java
+++ ficheros/proyectos/tareaMunicipios/src/App.java
@@ -8,18 +8,26 @@ public class App {
   public static void main(String[] args) throws Exception {
     ArrayList<Municipio> municipios = new ArrayList<>();
 
-    for (int i = 1996; i < 2022; i++) {
+    // Lectura de todos los municipios
+    for (int i = 1996; i <= 2022; i++) {
       municipios.addAll(LeerInformacion.LeerFicheroMunicipio(i));
     }
 
+    System.out.println("---MUNICIPIOS ORDENADES POR NOMBRE Y AÑO---");
     System.out.println(municipios.toString());
+
+    System.out.println("---INFO MIJAS EN 2022---");
     System.out.println(LeerInformacion.buscarMunicipio(municipios, "Mijas",
-        1998).toString());
+        2022).toString());
 
-    HashMap<String, Integer> incrementos = LeerInformacion.IncrementoPoblacion(municipios, 2000, 2005);
-    System.out.println(incrementos.get("Fuengirola"));
-    System.out.println(incrementos.get("Mijas"));
+    System.out.println("---INCREMENTOS (2016 - 2022)---");
+    HashMap<String, Integer> incrementos = LeerInformacion.IncrementoPoblacion(municipios, 2016, 2022);
 
+    System.out.println("Incremento de Fuengirola: " + incrementos.get("Fuengirola"));
+    System.out.println("Incremento de Mijas: " + incrementos.get("Mijas"));
+
+    System.out.println();
+    System.out.println("---MUNICIPIO MENOS POBLADO EN 2021---");
     municipios = LeerInformacion.LeerFicheroMunicipio(2021);
     Municipio menorMunicipio = municipios.get(0);
     int menorPoblacion = menorMunicipio.getPoblacion();
