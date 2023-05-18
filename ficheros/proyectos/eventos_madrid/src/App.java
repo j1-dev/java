ec86da2360e6b2d5e2783a4baf57de5e92d80897
j1-dev <aephyxen@gmail.com>
2023-05-18 19:49:10 +0200

    18 may

--- ficheros/proyectos/eventos_madrid/src/App.java
+++ ficheros/proyectos/eventos_madrid/src/App.java
@@ -4,13 +4,64 @@ import clases.Evento;
 import utils.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        // ArrayList<Evento> eventosJson =
-        // jsonUtils.ReadEventosFromJsonFile("src/recursos/eventos.json");
-        ArrayList<Evento> eventosCsv = csvUtils.readEventosFromCsvFile("src/recursos/eventos.csv");
+  public static void main(String[] args) throws Exception {
+    ArrayList<Evento> eventosJson = jsonUtils.ReadEventosFromJsonFile("src/recursos/eventos.json");
+    ArrayList<Evento> eventosCsv = csvUtils.readEventosFromCsvFile("src/recursos/eventos.csv");
+    ArrayList<String> codigosPostales = new ArrayList<>(0);
 
-        // eventosJson.forEach((e) -> System.out.println(e.toString()));
-        // eventosCsv.forEach((e) -> System.out.println(e.toString()));
+    eventosJson.forEach(evento -> {
+      String codigoPostal = evento.getAddress().getArea().getPostalCode();
+      if (!codigoPostal.equals("")) {
+        if (!codigosPostales.contains(codigoPostal)) {
+          System.out.println(codigoPostal);
+          codigosPostales.add(evento.getAddress().getArea().getPostalCode());
+        }
+      }
+    });
 
-    }
+    // eventosJson.forEach((e) -> System.out.println(e.toString()));
+    // eventosCsv.forEach((e) -> System.out.println(e.toString()));
+
+    System.out.println("====LISTADO DE EVENTOS GRATUITOS====");
+    System.out.println(" ---Json");
+
+    eventosJson.forEach(evento -> {
+      if (evento.getFree() == 1) {
+        System.out.println(evento.toString());
+      }
+    });
+
+    System.out.println(" ---Csv");
+
+    eventosCsv.forEach(evento -> {
+      if (evento.getFree() == 1) {
+        System.out.println(evento.toString());
+      }
+    });
+
+    int random = (int) (Math.random() * codigosPostales.size());
+
+    System.out.println(random + " - " + codigosPostales.size());
+
+    String codigo = codigosPostales.get(random);
+
+    System.out.println("====LISTADO DE EVENTOS GRATUITOS EN EL CODIGO POSTAL: " + codigo);
+    System.out.println("  ---Json");
+
+    eventosJson.forEach(evento -> {
+      if (evento.getFree() == 1 && evento.getAddress().getArea().getPostalCode() == codigo) {
+        System.out.println(evento.toString());
+      }
+    });
+    System.out.println("  ---Csv");
+
+    eventosCsv.forEach(evento -> {
+      if (evento.getFree() == 1 && evento.getAddress().getArea().getPostalCode() == codigo) {
+        System.out.println(evento.toString());
+      }
+    });
+
+    System.out.println(eventosJson.size());
+    System.out.println(eventosCsv.size());
+  }
 }
